c093bbe878e8e9eaf204220a543ed1da73d2cf8b
mike <mike.wuestenberg@gmail.com>
2020-11-25 15:00:36 +0100

    fixed hadoop path

--- docker/docker-down.sh
+++ docker/docker-down.sh
@@ -1,6 +1,6 @@
 docker-compose -f kafka/docker-compose-kafka.yml down
 
-docker-compose -f hadoop/docker-compose-hadoop.yml down
+docker-compose -f hdfs/docker-compose-hadoop.yml down
 
 docker-compose -f spark/docker-compose-spark.yml down
 
--- docker/docker-up.sh
+++ docker/docker-up.sh
@@ -1,6 +1,6 @@
 docker-compose --env-file ./.env -f kafka/docker-compose-kafka.yml up -d
 
-docker-compose --env-file ./.env -f hadoop/docker-compose-hadoop.yml up -d
+docker-compose --env-file ./.env -f hdfs/docker-compose-hadoop.yml up -d
 
 docker-compose --env-file ./.env -f spark/docker-compose-spark.yml up -d
 
