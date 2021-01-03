495b9c73a2da09e8567d62fed56e56c234539de8
Akardian <mike.wuestenberg@gmail.com>
2021-01-03 03:25:05 +0100

    add history server settings

--- docker/docker-up.sh
+++ docker/docker-up.sh
@@ -7,6 +7,10 @@ echo Create log directories on $HDFS_CONTAINER
 docker exec $HDFS_CONTAINER hdfs dfs -mkdir -p ../haw/shared/event-logs
 docker exec $HDFS_CONTAINER hdfs dfs -mkdir -p ../haw/shared/event-logs
 
+echo Starting History server
+SPARK_CONTAINER=$(docker ps -q -n 1 -f name=spark_master*)
+docker exec -it spark_master_1 sbin/start-history-server.sh
+
 docker-compose --env-file ./.env -f spark/docker-compose-spark.yml up -d
 docker-compose --env-file ./.env -f druid/docker-compose-druid.yml up -d
 docker-compose --env-file ./.env -f dashboard/docker-compose-dasboard.yml up -d
