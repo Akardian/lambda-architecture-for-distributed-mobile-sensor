55a06bd1c1ad0805bd7b88941642e0f5fded6f38
Akardian <mike.wuestenberg@gmail.com>
2021-01-03 02:55:04 +0100

    add history server settings

--- docker/docker-up.sh
+++ docker/docker-up.sh
@@ -1,5 +1,10 @@
 docker-compose --env-file ./.env -f kafka/docker-compose-kafka.yml up -d
 docker-compose --env-file ./.env -f hdfs/docker-compose-hadoop.yml up -d
+
+HDFS_CONTAINER=$(docker ps -q -n 1 -f name=hdfs_datanode*)
+docker exec $HDFS_CONTAINER hdfs dfs -mkdir -p user/haw/shared/logs
+docker exec $HDFS_CONTAINER hdfs dfs -mkdir -p user/haw/shared/history
+
 docker-compose --env-file ./.env -f spark/docker-compose-spark.yml up -d
 docker-compose --env-file ./.env -f druid/docker-compose-druid.yml up -d
 docker-compose --env-file ./.env -f dashboard/docker-compose-dasboard.yml up -d
