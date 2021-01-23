4008ecdda49c42ad4a3f2a37d84bda012a8835d5
Mike <mike.wuestenberg@haw-hamburg.de>
2021-01-23 11:31:32 +0100

    column renamed

--- src/Spark/sparkfind3/src/main/scala/SparkFind3.scala
+++ src/Spark/sparkfind3/src/main/scala/SparkFind3.scala
@@ -58,7 +58,7 @@ object SparkFind3 {
         
         //Create timestamp for HDS partition(Remove not allowed characters for HDFS)
         val hdfsDataFrame = avroDataFrame
-            .withColumn("timestamp-hdfs", date_format(date_trunc("hour", $"timestamp-KafkaIn"), "yyyy-MM-dd HH-mm"))
+            .withColumn("timestamp-hdfs", date_format(date_trunc("hour", $"timestamp-Kafka-in"), "yyyy-MM-dd HH-mm"))
 
         //Write RAW data to HDFS
         hdfsDataFrame.writeStream  
