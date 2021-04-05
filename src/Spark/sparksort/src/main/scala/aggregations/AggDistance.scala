db8d755d47fab1c09a8330e9df7cb30364f5157c
Akardian <mike.wuestenberg@gmail.com>
2021-04-05 02:29:23 +0200

    distance calculation

--- src/Spark/sparksort/src/main/scala/aggregations/AggDistance.scala
+++ src/Spark/sparksort/src/main/scala/aggregations/AggDistance.scala
@@ -14,7 +14,7 @@ object  AggDistance extends Aggregator[OdomPoint, BufferPoints, Double] {
     def zero: BufferPoints = {
         log.warn(DEBUG_MSG_AVG + "##### AggDistance zero #####")
         
-        val buffer = BufferPoints(SortedSet.empty)
+        val buffer = BufferPoints(SortedSet())
 
         log.warn(DEBUG_MSG_AVG + buffer)
         buffer
