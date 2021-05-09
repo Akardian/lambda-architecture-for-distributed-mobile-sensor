221094a673e741a76493a105553b3e0de52fb1a8
Akardian <mike.wuestenberg@gmail.com>
2021-05-10 01:07:39 +0200

    path correction

--- run/sparkExperimental/sparkExperimental-submit.sh
+++ run/sparkExperimental/sparkExperimental-submit.sh
@@ -4,7 +4,7 @@ SPARK_CONTAINER=$(docker ps -q -n 1 -f name=spark_master*)
 
 JAR_NAME=SparkExperimental-0.1.jar
 
-FAT_JAR_PATH=../../src/Spark/sparkExperimental/target/scala-2.12/$JAR_NAME
+FAT_JAR_PATH=../../src/spark/sparkExperimental/target/scala-2.12/$JAR_NAME
 
 HDFS_PATH=../haw/spark-jars
 HDFS_JAR=tmp/$JAR_NAME
