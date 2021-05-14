4ff0d2f1ccaeb4bfb8c07d57d0fc58d68d346fb9
Akardian <mike.wuestenberg@gmail.com>
2021-05-14 06:19:21 +0200

    removed Rolling Avg

--- src/spark/sparkFind3/src/main/scala/transformations/TransWifi.scala
+++ src/spark/sparkFind3/src/main/scala/transformations/TransWifi.scala
@@ -27,29 +27,4 @@ object TransWifi {
         )
         avgWifi.drop(col(wifiColumn))
     }
-
-    /**
-      * This is a test method to calculate the total running Avergage (Use with caution)
-      *
-      * @param spark SparkSession
-      * @param dataframe dataframe with timestamp and wifiData
-      * @param timestampColumn Timestamp column name
-      * @param averageWifiColumn average wifi column name
-      * @return Dataframe of timestamp and running average
-      */
-    def runningAverage(spark: SparkSession, dataframe: DataFrame, timestampColumn: String, averageWifiColumn: String) : DataFrame = {
-        import spark.implicits._
-
-        val rollingAvg = dataframe
-            .select(col(N_TIMESTAMP_KAFKA_IN).as("timestamp"), col(N_AVG_WIFI).as("wifiAvg"))
-            .as[WifiData]
-
-        // Convert the function to a `TypedColumn` and give it a name
-        val averageSalary = AggRollingAvg.toColumn.name("rollingAvg")
-
-        rollingAvg
-            .select(averageSalary)
-            .select(explode('list))
-            .select($"col._1".as("timestamp"), $"col._2".as("sum"))
-    }
 }
