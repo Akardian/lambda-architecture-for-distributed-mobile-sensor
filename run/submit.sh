27151632d9ebc8868d40fb9340b34108ef55b99d
Akardian <mike.wuestenberg@gmail.com>
2021-06-21 21:45:32 +0200

    copy data from new to tmp

--- run/submit.sh
+++ run/submit.sh
@@ -24,7 +24,7 @@ then
     echo Streaming Application
 else
     echo Is a batch Application
-    driverid=grep -Po driver-[0-9]+-[0-9]+ output | head 1
+    driverid=cat output | grep -Po driver-[0-9]+-[0-9]+ | head -1
     echo Submission ID is [$driverid]
     docker exec $SPARK_CONTAINER /opt/bitnami/spark/bin/spark-submit --kill $driverid --master spark://master:7077
     
